3aab8c6601e4be2f482def779f846a766f7d4ee8
ColtAllen <10178857+ColtAllen@users.noreply.github.com>
2021-04-27 07:23:03 -0600

    Added DB creation SQL statements

--- src/db_module.py
+++ src/db_module.py
@@ -10,7 +10,7 @@ def create_db_tables(db_path):
     """Create all SQLite tables.
 
     Args:
-        db_path: A string containing the full directory and database name
+        db_path: A string containing the full directory and database name.
     """
 
     con = sqlite3.connect(db_path)
@@ -24,30 +24,146 @@ def create_db_tables(db_path):
 
     #Create the table
     con.executescript("""
-        DROP TABLE IF EXISTS ;
-        DROP TABLE IF EXISTS ;
-        DROP TABLE IF EXISTS ;
-        DROP TABLE IF EXISTS ;
-        DROP TABLE IF EXISTS ;
-        DROP TABLE IF EXISTS ;
-        DROP TABLE IF EXISTS ;
-        DROP TABLE IF EXISTS ;
-        DROP TABLE IF EXISTS ;
-        DROP TABLE IF EXISTS ;
-        DROP TABLE IF EXISTS ;
+        DROP TABLE IF EXISTS rescuetime;
+        DROP TABLE IF EXISTS remarkable;
+        DROP TABLE IF EXISTS fitness;
+        DROP TABLE IF EXISTS nutrition;
+        DROP TABLE IF EXISTS exist_tags;
+        DROP TABLE IF EXISTS exist_journal;
+        DROP TABLE IF EXISTS exist_time;
+        DROP TABLE IF EXISTS exist_fitness;
+        DROP TABLE IF EXISTS mood_charts;
+        DROP TABLE IF EXISTS bullet_journal;
+        DROP VIEW IF EXISTS journal_view;
 
-        CREATE TABLE rescuetime_emails (
+        CREATE TABLE rescuetime (
+            date text PRIMARY KEY,
+            prd_hours float NOT NULL,
+            dst_hours float NOT NULL,
+            neut_hours float NOT NULL
+        );
+
+        CREATE TABLE remarkable (
+            date text PRIMARY KEY,
+            mood float NOT NULL,
+            entry text NOT NULL
+        );
+
+        CREATE TABLE fitness (
+            date text PRIMARY KEY,
+            weight float,
+            bmr float,
+            pulse float,
+            sleep float,
+            deep_sleep float,
+            light_sleep float,
+            rem_sleep float,
+            awakes float,
+            daily_steps float,
+            calories_out float
+        );
+
+        CREATE TABLE nutrition (
+            date text PRIMARY KEY,
+            calories float,
+            total_fat float,
+            total_carbs float,
+            protein float,
+            trans_fat float,
+            sat_fat float,
+            sodium float,
+            net_carbs float,
+            fiber float
+        );
+
+        CREATE TABLE exist_tags (
+            date text PRIMARY KEY,
+            alcohol integer,
+            bedsheets integer,
+            cardio integer,
+            cleaning integer,
+            drawing integer,
+            eating_out integer,
+            fasting integer,
+            guitar integer,
+            laundry integer,
+            learning integer,
+            meal_prep integer,
+            meditation integer,
+            nap integer,
+            nutribullet integer,
+            piano integer,
+            powerdrive integer,
+            reading integer,
+            shopping integer,
+            tech integer,
+            travel integer,
+            tv integer,
+            walk integer,
+            writing integer
+        );
+
+        CREATE TABLE exist_journal (
+            date text PRIMARY KEY,
+            mood float,
+            entry text
+        );
+
+        CREATE TABLE exist_time (
             date text PRIMARY KEY,
             prd_hours float,
             dst_hours float,
             neut_hours float
         );
 
-        CREATE TABLE journal_emails (
+        CREATE TABLE exist_fitness (
+            date text PRIMARY KEY,
+            active_cal float,
+            pulse float,
+            pulse_max float,
+            pulse_rest float,
+            steps float,
+            weight float,
+            sleep float,
+            sleep_end float,
+            sleep_start float,
+        );
+
+        CREATE TABLE mood_charts (
             date text PRIMARY KEY,
             mood float,
-            entry text;
+            sleep float,
+            cardio integer,
+            meditate integer,
+            mood_note text
         );
+
+        CREATE TABLE bullet_journal (
+            date text PRIMARY KEY,
+            mood float,
+            sleep float,
+            steps float,
+            cardio integer,
+            meditate integer,
+            mood_note text,
+            fasting integer,
+            cheat_meals integer,
+            read integer,
+            draw integer,
+            learn integer,
+            write integer,
+            guitar integer
+        );
+
+        CREATE VIEW journal_view (
+            SELECT date, (mood-4)/3 as mood, mood_note FROM mood_chart
+            UNION
+            SELECT date, (mood-5)/4 as mood, mood_note FROM bullet_journal
+            UNION
+            SELECT date, (mood-3)/2 as mood, entry FROM exist_journal
+            UNION
+            SELECT date, (mood-5)/4 as mood, entry FROM remarkable
+        )
 
     """)
 
