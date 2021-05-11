7d95bc1e0a3a3724667157b4666e06da59cb1f23
ColtAllen <10178857+ColtAllen@users.noreply.github.com>
2021-05-10 20:00:50 -0600

    Working version of journal calendar.

--- src/plotly_charts.py
+++ src/plotly_charts.py
@@ -1,57 +1,56 @@
-# MIT LICENSE
-
 import datetime
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 import plotly.express as px
 import numpy as np
 
-def display_year(z,
-                 year: int = None,
+def display_year(journal_tuples: list,
+                 year: int,
                  month_lines: bool = True,
                  fig=None,
                  row: int = None):
+
+    days_in_year = [datetime.datetime.strptime(tup[0],'%Y-%m-%d') for tup in journal_tuples]
+    daily_moods = [tup[1] for tup in journal_tuples]
+    daily_entries = [tup[2] for tup in journal_tuples]
+
+    # TODO: Add datetime formatting to fill out remaining days of year with nulls, and add line breaks for entry hovertext
     
-    if year is None:
-        year = datetime.datetime.now().year
-    
-    data = np.ones(365) * np.nan
-    data[:len(z)] = z
+    #data = np.ones(len(days_in_year)) * np.nan
+    #data[:len(daily_moods)] = daily_moods
+
+    # data = np.ones(365) * np.nan
+    # data[:len(z)] = z
     
 
-    d1 = datetime.date(year, 1, 1)
-    d2 = datetime.date(year, 12, 31)
+    # d1 = datetime.date(year, 1, 1)
+    # d2 = datetime.date(year, 12, 31)
 
-    delta = d2 - d1
+    # delta = d2 - d1
+
+    
     
     month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     month_days =   [31,    28,    31,     30,    31,     30,    31,    31,    30,    31,    30,    31]
     month_positions = (np.cumsum(month_days) - 15)/7
 
-    dates_in_year = [d1 + datetime.timedelta(i) for i in range(delta.days+1)] #gives me a list with datetimes for each day a year
-    weekdays_in_year = [i.weekday() for i in dates_in_year] #gives [0,1,2,3,4,5,6,0,1,2,3,4,5,6,…] (ticktext in xaxis dict translates this to weekdays
+    #dates_in_year = [d1 + datetime.timedelta(i) for i in range(delta.days+1)] #gives me a list with datetimes for each day a year
+    weekdays_in_year = [i.weekday() for i in days_in_year] #gives [0,1,2,3,4,5,6,0,1,2,3,4,5,6,…] (ticktext in xaxis dict translates this to weekdays
     
     weeknumber_of_dates = [int(i.strftime("%V")) if not (int(i.strftime("%V")) == 1 and i.month == 12) else 53
-                           for i in dates_in_year] #gives [1,1,1,1,1,1,1,2,2,2,2,2,2,2,…] name is self-explanatory
-    text = [str(i) for i in dates_in_year] #gives something like list of strings like ‘2018-01-25’ for each date. Used in data trace to make good hovertext.
-    #4cc417 green #347c17 dark green
-    colorscale=[[False, '#eeeeee'], [True, '#76cf63']]
-    colorscale=px.colors.diverging.RdBu
-    
-    # handle end of year
-    
+                           for i in days_in_year] #gives [1,1,1,1,1,1,1,2,2,2,2,2,2,2,…] name is self-explanatory
 
     data = [
         go.Heatmap(
             x=weeknumber_of_dates,
             y=weekdays_in_year,
-            z=data,
-            text=text,
+            z=daily_moods,
+            text=days_in_year,
             hoverinfo='text',
             xgap=3, # this
             ygap=3, # and this is used to make the grid-like apperance
             showscale=False,
-            colorscale=colorscale
+            colorscale=px.colors.diverging.RdBu
         )
     ]
     
@@ -66,7 +65,7 @@ def display_year(z,
             hoverinfo='skip'
             
         )
-        for date, dow, wkn in zip(dates_in_year,
+        for date, dow, wkn in zip(days_in_year,
                                   weekdays_in_year,
                                   weeknumber_of_dates):
             if date.day == 1:
@@ -122,14 +121,24 @@ def display_year(z,
         fig.update_xaxes(layout['xaxis'])
         fig.update_yaxes(layout['yaxis'])
 
-    
     return fig
 
 
-def display_years(z, years):
+def journal_calendar(journal_tuples):
+    """
+
+    Args:
+        journal_tuples:
+    Returns:
+        fig:
+    """
+
+    years = list(set([int(tup[0].split('-')[0]) for tup in journal_tuples]))
+
     fig = make_subplots(rows=len(years), cols=1, subplot_titles=years)
+    
     for i, year in enumerate(years):
-        data = z[i*365 : (i+1)*365]
-        display_year(data, year=year, fig=fig, row=i)
+        data = [tup for tup in journal_tuples if int(tup[0].split('-')[0]) == year]#[i*365 : (i+1)*365]
+        display_year(data, year=year, fig=fig,row=i)
         fig.update_layout(height=250*len(years))
     return fig
